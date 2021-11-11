0d00e9941495ce65da8c5b1a1ea5449db129b553
Lary SENE <l.sene@canal-plus.com>
2021-11-11 08:05:00 -0400

    scrollbar ok

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,15 +3,15 @@ import Rive, { Layout } from 'rive-react';
 import Online from '../Online';
 const About = () => <div className="About">
     <div className="r1">
-    <Online />
+        <Rive src="assets/appstart.riv" /*layout={new Layout({ fit: 'fitWidth', alignment: 'center' })} */ />
     </div>
-    <Online />
-    <div className="r4">
+    <div className="r2">
+        <Online />
+    </div>
+    <div className="r3">
         Node.js <span id="node-version"></span>,
         Chromium <span id="chrome-version"></span>,
         Electron <span id="electron-version"></span>
     </div>
 </div>
 export default About;
-
-//<Rive src="assets/appstart.riv" layout={new Layout({ fit: 'fitWidth', alignment: 'center' })} />
