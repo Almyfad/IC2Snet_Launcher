18568171e1d32ba9d15a3552dfbfd727207753bb
Lary SENE <l.sene@canal-plus.com>
2021-11-14 01:26:54 -0400

    ...

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
 import './style.css'
 
 
 
 const Online = () => {
 
-  //  const [data, setData] = useState([]);
+  /*  const [data, setData] = useState([]);
     useEffect(() => {
 
-    }, []);
+    }, []);*/
     return (
         <div className="img"><img alt="underconstruction" src="./assets/img/underconstruction.jpg"/></div>
     )
