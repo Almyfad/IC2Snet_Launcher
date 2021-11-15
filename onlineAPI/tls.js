f95f9450b47f4a2f1e5d3af221e0d9b301574a90
Lary SENE <l.sene@canal-plus.com>
2021-11-14 22:43:22 -0400

    Update tls.js

---
+++ onlineAPI/tls.js
@@ -0,0 +1,21 @@
+const tls = require('tls');
+const fs = require('fs');
+ 
+const options = {
+    key: fs.readFileSync('./certs/server/server.key'),
+    cert: fs.readFileSync('./certs/server/server.crt'),
+    ca: fs.readFileSync('./certs/ca/ca.crt'), // authority chain for the clients
+    requestCert: true, // ask for a client cert
+    //rejectUnauthorized: false, // act on unauthorized clients at the app level
+};
+ 
+const server = tls.createServer(options, (socket) => {
+  console.log('server connected',
+              socket.authorized ? 'authorized' : 'unauthorized');
+  socket.write('welcome!\n');
+  socket.setEncoding('utf8');
+  socket.pipe(socket);
+});
+server.listen(8000, () => {
+  console.log('server bound');
+});
