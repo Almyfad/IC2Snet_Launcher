5582c36c6f267fd4114b4777bcb4e75bb35ef557
Lary SENE <l.sene@canal-plus.com>
2021-11-14 23:52:10 -0400

    Update tls.js

--- onlineAPI/tls.js
+++ onlineAPI/tls.js
@@ -1,6 +1,6 @@
 const tls = require('tls');
 const fs = require('fs');
- 
+
 const options = {
 
     key: fs.readFileSync('./certs/server/server.key'),
@@ -9,34 +9,37 @@ const options = {
     requestCert: true, // ask for a client cert
     //rejectUnauthorized: false, // act on unauthorized clients at the app level
 };
- 
+
 const server = tls.createServer(options, (socket) => {
-  console.log('server connected',
-              socket.authorized ? 'authorized' : 'unauthorized');
-  socket.write('welcome!\n');
-  socket.setEncoding('utf8');
-  socket.pipe(socket);
+    console.log('server connected',
+        socket.authorized ? 'authorized' : 'unauthorized');
+    socket.write('welcome!\n');
+    socket.setEncoding('utf8');
+    socket.pipe(socket);
+
+
+    socket.on('data', (data) => {
+        console.log(data)
+    });
 });
 
 
 
-const cb = () => {console.log("cb is called");}
+const cb = () => { console.log("cb is called"); }
 
 const tlsSessionStore = {};
 server.on('newSession', (id, data, cb) => {
     console.log(data)
-  tlsSessionStore[id.toString('hex')] = data;
-  cb();
+    tlsSessionStore[id.toString('hex')] = data;
+    cb();
 });
 server.on('resumeSession', (id, cb) => {
-  cb(null, tlsSessionStore[id.toString('hex')] || null);
+    cb(null, tlsSessionStore[id.toString('hex')] || null);
 });
 
 
-server.on('data', (data) => {
-   console.log(data)
-  });
+
 
 server.listen(2323, () => {
-  console.log('server bound');
+    console.log('server bound');
 });
